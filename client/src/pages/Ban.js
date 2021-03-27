229c3bf8bc47eae250e1a1b379311f8358e6d873
Saif Ul Islam <saifulislam84210@gmail.com>
2021-03-27 13:56:07 +0500

    add ban page, finish modifying te ban page

--- client/src/pages/Ban.js
+++ client/src/pages/Ban.js
@@ -1,29 +1,52 @@
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Box, Container } from '@material-ui/core';
 import BanListResults from 'src/components/ban/BanListResults';
 import BanListToolbar from 'src/components/ban/BanListToolbar';
-import customers from 'src/__mocks__/customers';
 
-const BanList = () => (
-  <>
-    <Helmet>
-      <title>Bans</title>
-    </Helmet>
-    <Box
-      sx={{
-        backgroundColor: 'background.default',
-        minHeight: '100%',
-        py: 3
-      }}
-    >
-      <Container maxWidth={false}>
-        <BanListToolbar />
-        <Box sx={{ pt: 3 }}>
-          <BanListResults customers={customers} />
-        </Box>
-      </Container>
-    </Box>
-  </>
-);
+// API
+import API from 'src/API/API';
+
+const BanList = () => {
+  const [dataReady, setDataReady] = React.useState(false);
+  const finalObjectsToSend = [];
+
+  React.useEffect(async () => {
+    const response = await API.getRequest('http://localhost/forbidden');
+
+    response.domains.forEach((item) => {
+      finalObjectsToSend.push({
+        name: '-',
+        email: '-',
+        domainName: item,
+        ip: '-',
+        addedDate: Date(),
+      });
+    });
+    setDataReady(true);
+  });
+
+  return (
+    <>
+      <Helmet>
+        <title>Bans</title>
+      </Helmet>
+      <Box
+        sx={{
+          backgroundColor: 'background.default',
+          minHeight: '100%',
+          py: 3
+        }}
+      >
+        <Container maxWidth={false}>
+          <BanListToolbar />
+          <Box sx={{ pt: 3 }}>
+            {dataReady && (<BanListResults customers={finalObjectsToSend} />)}
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 export default BanList;
